a114804b9f8919de29ef2508308fd7e249dae44c
Michael Kriese <michael.kriese@visualon.de>
2015-05-09 15:07:55 +0200

    Added flag to build runtime compatible libraries.

--- build.ps1
+++ build.ps1
@@ -1,4 +1,9 @@
 
+param(
+    [Alias("c")]
+    [switch] $compatible
+)
+
 $msbuild = "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
 
 $out = Resolve-Path .
@@ -23,4 +28,8 @@ $args += "runtime\CSharp\Antlr4.Runtime\Antlr4.Runtime.vs2013.csproj"
 Invoke-MSBuild -tfv "v2.0" -const "TRACE;"
 Invoke-MSBuild -tfv "v3.5" -const "TRACE;NET35PLUS"
 Invoke-MSBuild -tfv "v4.0" -const "TRACE;NET35PLUS;NET40PLUS"
-Invoke-MSBuild -tfv "v4.5" -const "TRACE;NET35PLUS;NET40PLUS;NET45PLUS"
+if ($compatible){
+    Invoke-MSBuild -tfv "v4.5" -const "TRACE;NET35PLUS;NET40PLUS"
+} else {
+    Invoke-MSBuild -tfv "v4.5" -const "TRACE;NET35PLUS;NET40PLUS;NET45PLUS"
+}
